7ff4a8ff83cb2e173a36f4b4da03f82d052d3e5e
Srinidhi M <65710007+srinidhim2@users.noreply.github.com>
2023-03-08 22:08:27 +0530

    Create Medium_Longest_Palindromic_Subsequence.java
    Signed-off-by: Srinidhi M <65710007+srinidhim2@users.noreply.github.com>

---
+++ Medium_Longest_Palindromic_Subsequence.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int lscTab(String s1,String s2,int[][] dp){
+        int n=s1.length();
+        int m=s2.length();
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int j=0;j<=m;j++) dp[0][j]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                Character c1=s1.charAt(i-1);
+                Character c2=s2.charAt(j-1);
+            if(c1.equals(c2)){
+              
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else
+                dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+
+    }
+    public int longestPalinSubseq(String s1)
+    {
+        //code here
+        StringBuffer s2 =new StringBuffer(s1);
+        s2.reverse();
+        int a=s1.length(),b=s2.length();
+        int[][] dp1=new int[a+1][b+1];
+        // for(int[] arr:dp1) Arrays.fill(arr,-1);
+        return lscTab(s1,s2.toString(),dp1);
+    }
+}
