3b5fa19da8933aee27e77cf7367ea116f5ef7ff9
Srinidhi M <65710007+srinidhim2@users.noreply.github.com>
2023-03-12 22:05:56 +0530

    Update Medium_WildCard.java
    Signed-off-by: Srinidhi M <65710007+srinidhim2@users.noreply.github.com>

--- Medium_WildCard.java
+++ Medium_WildCard.java
@@ -1,6 +1,28 @@
+//{ Driver Code Starts
 import java.util.*;
-public class Solution {
-	public static int wild(int i,int j,String s1,String s2,int[][] dp){
+
+class WildcardPattern
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String pat = sc.nextLine();
+			String text = sc.nextLine();
+			Solution g = new Solution();
+			System.out.println(g.wildCard(pat,text));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{   public static int wild(int i,int j,String s1,String s2,int[][] dp){
         if(j<0 && i<0)
             return 1;
         if(i<0 && j>=0) return 0;
@@ -12,18 +34,30 @@ public class Solution {
         }
         if(dp[i][j]!=-1)    return dp[i][j];
         if(s1.charAt(i)==s2.charAt(j) || s1.charAt(i)=='?'){ 
-            return wild(i-1, j-1, s1, s2,dp);
+            return dp[i][j]=wild(i-1, j-1, s1, s2,dp);
         }
-        if(s1.charAt(i)=='*')   return wild(i-1, j, s1, s2,dp)==1 || wild(i, j-1, s1, s2,dp)==1 ?1:0;
-        return 0;
+        if(s1.charAt(i)=='*'){   
+            if(wild(i-1, j, s1, s2,dp)==1 || wild(i, j-1, s1, s2,dp)==1 )
+            {
+                dp[i][j]=1;
+                
+            
+            }
+            else dp[i][j]=0;
+            return wild(i-1, j, s1, s2,dp)==1 || wild(i, j-1, s1, s2,dp)==1 ?1:0;
+        
+            
+        }
+        return dp[i][j]=0;
     }
-	public static boolean wildcardMatching(String s1, String s2) {
-		// Write your code here.
-		int a=s1.length(),b=s2.length();
+    int wildCard(String s1, String s2)
+    {
+        // Your code goes here
+        int a=s1.length(),b=s2.length();
         int[][] dp=new int[a][b];
 
         for(int[] i:dp) Arrays.fill(i,-1);
 
-        return wild(a-1, b-1, s1, s2,dp)==1?true:false;
-	}
+        return wild(a-1, b-1, s1, s2,dp);
+    }
 }
